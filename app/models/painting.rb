5701d6072da9e924709b92fba94ab49c947c5fb9
Fabian <fabrofabian@gmail.com>
2019-05-23 00:00:54 -0700

    Done

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,29 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    total = 0
+    @@all.each {|p|
+      total += p.price
+    }
+    total
+  end
 end
+
+#gem uninstall pry-byebug
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,26 @@
 require_relative '../config/environment.rb'
 
+gen = Artist.new("Gen", 5)
+hirohiko = Artist.new("hirohiko", 35)
+masuda = Artist.new("Masuda", 20)
+winry = Artist.new("Winry", 8)
+yin = Artist.new("Yin", 2)
+vaire = Artist.new("Vaire", 13)
 
+anime = Gallery.new("Anime Gallery", "Tokyo")
+sculpt = Gallery.new("Ice Sculptures Center", "Ice Place")
+ceramic = Gallery.new("Ceramics Deck", "San Diego")
+sandart = Gallery.new("Beaches Cove", "Honolulu")
+comic = Gallery.new("Historical Comics", "LA")
 
+fire = Painting.new("Fire", 2500,  masuda, anime)
+waterfall = Painting.new("Waterfall falling", 5000, hirohiko, anime)
+trees = Painting.new("Forests Trees", 10000, vaire, comic)
+dolphins = Painting.new("Moar Dolphins", 240, winry, sculpt)
+doodles = Painting.new("Doodles for fun", 23, yin, comic)
+icepenguin = Painting.new("Ice Penguin", 432300, winry, sculpt)
+sandfortress = Painting.new("Sand Fortress Battlegrounds", 10, vaire, sandart)
+pottery = Painting.new("Extreme Pottery", 9000, gen, ceramic)
 
 binding.pry
 
